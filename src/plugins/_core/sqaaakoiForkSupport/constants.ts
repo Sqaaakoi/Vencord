c85f99993a37620ec3b992c949cc4cff576d10b5
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2024-11-19 01:34:11 +1300

    Update welcome message

--- src/plugins/_core/sqaaakoiForkSupport/constants.ts
+++ src/plugins/_core/sqaaakoiForkSupport/constants.ts
@@ -9,7 +9,7 @@ import { Devs } from "@utils/constants";
 
 export const SQAAAKOI_USER_ID = Devs.Sqaaakoi.id + "";
 
-export const CURRENT_WELCOME_NOTICE_VERSION = 3;
+export const CURRENT_WELCOME_NOTICE_VERSION = 4;
 export const WELCOME_NOTICE_VERSION_KEY = "SqaaakoiForkSupport_StartupMessageVersion";
 
 export const LAST_UPDATED_AT = new Date("2024-07-28T14:28:07.117Z");
@@ -17,6 +17,8 @@ export const LAST_UPDATED_AT = new Date("2024-07-28T14:28:07.117Z");
 // friends or not
 const F = (strings: TemplateStringsArray, ...args: any[]) => [true, String.raw(strings, ...args)] as [boolean, string];
 const N = (strings: TemplateStringsArray, ...args: any[]) => [false, String.raw(strings, ...args)] as [boolean, string];
+
+const LF = "\n";
 
 // horrible
 function friendsOnlyFilter(_template: TemplateStringsArray, ..._substitutions: (string | [boolean, string])[]): (isFriend: boolean) => string {
@@ -32,8 +34,7 @@ function friendsOnlyFilter(_template: TemplateStringsArray, ..._substitutions: (
                 if (Array.isArray(substitutions[i])) {
                     if (isFriend === substitutions[i][0]) out.push(substitutions[i][1]);
                 } else {
-                    // @ts-ignore
-                    out.push(substitutions[i]);
+                    out.push(substitutions[i] as string);
                 }
             }
         }
@@ -45,26 +46,18 @@ export const WELCOME_HEADER = "## 👋 Welcome to Sqaaakoi's Vencord fork!";
 export const WELCOME_BACK_HEADER = "## 👋 What's new in Sqaaakoi's Vencord fork";
 
 export const WELCOME_MESSAGE = friendsOnlyFilter`
-Many new plugins have been made and many existing plugins have been updated recently. Here's a showcase.
+It's been a few months since I updated this. Here's what I've been working on, and what's coming soon.
 
-**NewPluginsManager** shows a popup when plugins are added.
-The plugin is enabled by default, you can disable it if you find it annoying.
-Make sure to click Continue to completely dismiss it, this is intentional design.
+## Useful plugins
+**JunkCleanup** allows you to remove a lot of the annoyances in Discord. Includes 20+ toggles for you to choose what should be removed.
+**NewPluginsManager** automatically notifies you when new plugins and plugin settings are added. It is enabled by default.
+**BetterQuickReact** extends the context menu reactions list to include all the reactions you need.
 
-**AutoMute** now has options to automatically mute when someone who isn't a friend joins your voice channel.
+You might also want to try **AutomodIndicator**, **ProfileCommand**, and **PreSendModeration**.
 
-Additionally, **AskMeToMute** has been added so your admin friends can server mute you and it will turn into a client-side mute.
-
-For those people who often create servers, **ServerTemplatesList** inserts templates from all your servers into the server creation menu.
-
-**ShakingText** allows you to make \$shaking\$ text.
-
-Regarding plugins that haven't been added back, I either forgot about them, or didn't use them enough to bother fixing them.
-Some of these will be remade soon:tm:
--# ~~no they won't~~, or will they?
-
-That's all for now. I have a [list of planned plugins](https://github.com/users/Sqaaakoi/projects/3/views/1) that I'd recommend taking a look at!
+## Coming soon
+**VoiceChatTweaks** (name not final) which fixes issues with the voice channel UI and UX
 
 If any of my plugins have an issue, please file an issue on [GitHub](https://github.com/Sqaaakoi/Vencord/issues)
-${F`${"\n"}-# Thank you, friends! If you have feedback on these plugins, please DM me :)`}
+${F`${LF}-# Thank you, friends! If you have feedback on these plugins, please DM me :)`}
 `;
