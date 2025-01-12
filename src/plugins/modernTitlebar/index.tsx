2ef327d8d129ad9a1fa1efc8f85f1dd99995bd01
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2025-01-12 20:59:46 +1300

    ModernTitlebar: first version, unfinished

---
+++ src/plugins/modernTitlebar/index.tsx
@@ -0,0 +1,40 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import ErrorBoundary from "@components/ErrorBoundary";
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+import TitleBar from "./components/TitleBar";
+import { settings } from "./settings";
+
+export default definePlugin({
+    name: "ModernTitlebar",
+    description: "Adds a thicker, more modern looking titlebar to Discord, without the Visual Refresh",
+    authors: [Devs.Sqaaakoi],
+    settings,
+    patches: [
+        {
+            find: ".wordmarkWindows,",
+            replacement: {
+                match: /switch\(\i\)\{/,
+                replace: "return $self.renderTitleBar(arguments[0]);switch(0){"
+            }
+        },
+        {
+            find: 'setProperty("--custom-app-panels-height"',
+            replacement: {
+                match: /,\(0,\i\.jsx\).{0,30}?ACCOUNT_PANEL,.{0,30}?{}\)}\)/,
+                replace: ""
+            }
+        }
+    ],
+    renderTitleBar(props) {
+        return <ErrorBoundary noop>
+            <TitleBar {...props} />
+        </ErrorBoundary>;
+    }
+});
