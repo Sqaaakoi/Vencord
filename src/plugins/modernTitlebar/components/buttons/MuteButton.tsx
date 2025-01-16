2f3a2e0ec68e4a2de2527610abcc937254302771
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2025-01-17 02:44:45 +1300

    ModernTitlebar: Finish most of the mute button (speaking while muted tooltip timeout/dismiss when hovered and context menu not done yet)

--- src/plugins/modernTitlebar/components/buttons/MuteButton.tsx
+++ src/plugins/modernTitlebar/components/buttons/MuteButton.tsx
@@ -4,39 +4,76 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { findByCodeLazy } from "@webpack";
-import { useEffect, useState } from "@webpack/common";
+import { getIntlMessage } from "@utils/discord";
+import { findByCodeLazy, findByPropsLazy, findStoreLazy } from "@webpack";
+import { ChannelStore, Icons, Tooltip, useEffect, useStateFromStores } from "@webpack/common";
 
 import { cl } from "../TitleBar";
 import TitleBarButton from "../TitleBarButton";
 
 const LottieMuteIcon = findByCodeLazy('"hover_unmuted":"hover_muted"');
 
+const RTCConnectionStore = findStoreLazy("RTCConnectionStore");
+const MediaEngineStore = findStoreLazy("MediaEngineStore");
+const GameConsoleStore = findStoreLazy("GameConsoleStore");
+
+// horrible find. could be replaced by copying the useStateFromStores wrapper
+const useMuted = findByCodeLazy(/,impersonateStore:\i\.\i/);
+const toggleMute = findByCodeLazy("toggleSelfMute({location:", "#{intl::SUPPRESSED}");
+
+const classes = findByPropsLazy("strikethrough", "buildOverrideButton");
+
+const getTooltipLabel = findByCodeLazy("#{intl::CONSOLE_CONNECTING_DISABLED}", "#{intl::MUTE_ALT}");
+
 export default function MuteButton() {
     // Most of this was blatantly stolen from Discord's own button.
-    const [state, setState] = useState(false);
-    const selfMute = false;
-    const suppress = false;
-    const serverMute = false;
-    const muted = selfMute || suppress || serverMute || state;
+
+    const currentChannel = useStateFromStores([RTCConnectionStore, ChannelStore], () => {
+        const channelId = RTCConnectionStore.getChannelId();
+        return channelId != null ? ChannelStore.getChannel(channelId) : null;
+    });
+
+    const { mute: serverMute, selfMute, suppress } = useMuted(currentChannel);
+
+    const speakingWhileMuted = useStateFromStores([MediaEngineStore], () => MediaEngineStore.getSpeakingWhileMuted());
+
+    // I can't believe I am actually implementing this.
+    const awaitingRemote = useStateFromStores([GameConsoleStore], () => GameConsoleStore.getAwaitingRemoteSessionInfo() != null);
+
+    const muted = selfMute || suppress || serverMute;
 
     const { Component, play, events } = LottieMuteIcon(muted ? "unmute" : "mute");
     useEffect(() => () => play(), [muted, play]);
-    return <>
-        <TitleBarButton
-            action={() => setState(!state)}
+    return <Tooltip
+        text={speakingWhileMuted ? getIntlMessage("ACCOUNT_SPEAKING_WHILE_MUTED") : getTooltipLabel(selfMute, serverMute, suppress, awaitingRemote, false)}
+        position="bottom"
+        color={speakingWhileMuted ? Tooltip.Colors.GREEN : undefined}
+        forceOpen={speakingWhileMuted}
+    >
+        {tooltipProps => <TitleBarButton
+            action={() => { toggleMute(serverMute, suppress); tooltipProps.onClick(); }}
             className={cl("mute")}
             buttonProps={{
-                onMouseEnter: () => events.onMouseEnter(),
-                onMouseLeave: () => events.onMouseLeave(),
+                ...tooltipProps,
+                onMouseEnter: () => { tooltipProps.onMouseEnter(); events.onMouseEnter(); },
+                onMouseLeave: () => { tooltipProps.onMouseLeave(); events.onMouseLeave(); },
                 role: "switch",
                 "aria-checked": muted,
+                disabled: awaitingRemote
             }}
         >
-            <Component
-                size="custom"
-                color={muted ? "var(--status-danger)" : "currentColor"}
-            />
-        </TitleBarButton >
-    </>;
+            {serverMute || suppress ?
+                <Icons.MicrophoneDenyIcon
+                    size="custom"
+                    colorClass={classes.strikethrough}
+                    color="currentColor"
+                />
+                :
+                <Component
+                    size="custom"
+                    color={muted ? "var(--status-danger)" : "currentColor"}
+                />
+            }
+        </TitleBarButton>}
+    </Tooltip>;
 }
