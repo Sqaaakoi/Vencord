8f814f7217bf6ffe677568c5516d73a032adbb37
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2025-02-21 20:51:13 +1300

    ForwardHere: Forward to author

--- src/plugins/forwardHere/index.ts
+++ src/plugins/forwardHere/index.ts
@@ -7,6 +7,8 @@
 import { definePluginSettings } from "@api/Settings";
 import { Devs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
+import { UserStore } from "@webpack/common";
+import { Message } from "discord-types/general";
 
 export default definePlugin({
     name: "ForwardHere",
@@ -17,6 +19,11 @@ export default definePlugin({
             type: OptionType.BOOLEAN,
             description: "Selects the current channel in the forward menu by default",
             default: false
+        },
+        author: {
+            type: OptionType.BOOLEAN,
+            description: "Adds the author of the message to the forwarding menu",
+            default: false
         }
     }),
     patches: [
@@ -26,14 +33,19 @@ export default definePlugin({
                 // top of search results
                 {
                     match: /(selectedDestinations:)(\i)(,originDestination:)(\i)/,
-                    replace: "$1[$4,...$2]$3$4"
+                    replace: "$1[$4,...($self.injectAuthor(arguments[0].message)),...$2]$3$4"
                 },
                 // select by default
                 {
-                    match: /(\[\i,\i]=\i\.useState\()(\i)(\).{0,120}originDestination:)(\i)/,
+                    match: /(\[\i,\i]=\i\.useState\()(\i)(\).{0,200}?originDestination:)(\i)/,
                     replace: "$1$2.length<1&&$self.settings.store.selectedByDefault?[$4]:$2$3$4"
                 }
             ]
         }
-    ]
+    ],
+    injectAuthor(message: Message) {
+        const userId = message.author.id;
+        if (!this.settings.store.author || (UserStore.getCurrentUser().id === userId) || (message.webhookId && message.author.isNonUserBot())) return [];
+        return [{ type: "user", id: userId }];
+    }
 });
