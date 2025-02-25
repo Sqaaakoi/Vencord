99493fbf080182e7841f055990a86f0c8aaf836e
Sqaaakoi <sqaaakoi-git@sqaaakoi.xyz>
2025-02-26 11:16:01 +1300

    ModernTitlebar: Switch to Unified Styles API

--- src/plugins/modernTitlebar/components/TitleBar.tsx
+++ src/plugins/modernTitlebar/components/TitleBar.tsx
@@ -10,7 +10,6 @@ import { classes } from "@utils/misc";
 import { findStoreLazy } from "@webpack";
 import { useStateFromStores } from "@webpack/common";
 
-import OverrideCSS from "./OverrideCSS";
 import TitleBarGroupLeft from "./TitleBarGroupLeft";
 import TitleBarGroupRight from "./TitleBarGroupRight";
 
@@ -30,7 +29,6 @@ export default function TitleBar(props: {
     if (props?.windowKey ? (IS_VESKTOP ? !window?.Vesktop?.Settings?.store?.customTitleBar : false) : false) return null;
 
     return <div className={cl("container")}>
-        <OverrideCSS className={cl("styles")} />
         <div className={cl("titlebar")}>
             <TitleBarGroupLeft user={user} windowKey={props?.windowKey} />
             <div className={classes(cl("spacer"))} />
